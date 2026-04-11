1f25b7e2bdb488209fe92945a59ef9c8c8989449
Lenson200 <136510914+Lenson200@users.noreply.github.com>
2026-04-11 05:54:59 +0000

    adding media

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,9 +121,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+# Media files (User uploads)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# WhiteNoise for serving static and media files in production
+if not DEBUG:
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Security Settings for Production
 if not DEBUG:
