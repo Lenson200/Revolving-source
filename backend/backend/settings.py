9f0cda8633bfc333459253490daaef3dbf0fbd08
Lenson200 <136510914+Lenson200@users.noreply.github.com>
2026-04-11 05:59:14 +0000

    adding media3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,8 +130,8 @@ if not DEBUG:
     MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Security Settings for Production
 if not DEBUG:
