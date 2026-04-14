3a79d07ca616412355082bc8fa944538e4e0f63a
Lenson200 <136510914+Lenson200@users.noreply.github.com>
2026-04-14 19:22:07 +0000

    form8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 # Media files (User uploads)
 MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/"
-MEDIA_ROOT = 'app/media/'
+MEDIA_ROOT = '/app/media'
 
 
 
